ddea7cf90fd456f9b00ae8aba1a7b6a2c7634c7c
defineEuclidian <56561858+defineEuclidian@users.noreply.github.com>
2022-09-27 15:11:05 -0400

    Improvements
    Data is now reversed, allowing for a clearer regression line and actual predictions to be printed out
    Also added a parameter to the regression function allowing for no graphs to be produced

--- backend/algorithm_testing.py
+++ backend/algorithm_testing.py
@@ -2,21 +2,28 @@ import requests
 import pandas as pd
 import matplotlib.pyplot as plt
 
-def data_linear_regression_graph(name, figname):
+def data_linear_regression_graph(sym, name, graph, figname):
     partial_data = data[name]
 
     x = partial_data.size - 1
 
     avg_slope = 0
-    mean = partial_data.mean()
+    intercept = partial_data.mean()
 
     for i in range(x):
         avg_slope += partial_data[i + 1] - partial_data[i]
     avg_slope /= x
 
-    partial_data_plot = partial_data.plot()
-    partial_data_plot.plot([mean + avg_slope * i for i in range(x)])
-    partial_data_plot.get_figure().savefig(figname)
+    if graph:
+        partial_data_plot = partial_data.plot(figsize = (8, 5))
+        partial_data_plot.plot([intercept + avg_slope * i for i in range(x + 1)])
+        partial_data_plot.get_figure().savefig(figname)
+
+    deviation = round(partial_data.std(), 2)
+
+    prediction = round(intercept + avg_slope * (x + 1), 2)
+
+    print("(" + sym + ")", name, "next day:", "$" + str(prediction), "+-", "$" + str(deviation))
 
 
 ###### Get data from AlphaVantage
@@ -40,6 +47,9 @@ data = data.rename(lambda x: x[3:], axis = "columns")
 # Convert all string values to floats; we cannot work with numbers that are strings
 data = data.astype(float)
 
+# Reverse all data
+data = data.iloc[::-1]
+
 ###### Linear Regression WIP
 
-data_linear_regression_graph("close", "close_linreg")
+data_linear_regression_graph(symbol, "close", False, "")
