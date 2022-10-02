daa11ca23b5eda190a7ea9610844893da8f6bbe1
defineEuclidian <56561858+defineEuclidian@users.noreply.github.com>
2022-10-02 17:39:58 -0400

    Minor improvements to linear regression
    Replaced mean as the intercept with just the first stock price; should result in a more consistent regression line
    Volume gets rounded to whole number now
    Added comments to the regression function

--- backend/algorithm_testing.py
+++ backend/algorithm_testing.py
@@ -3,25 +3,37 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 def data_linear_regression_graph(sym, name, graph, figname):
+    # Cache Series from data DataFrame
     partial_data = data[name]
 
+    # Get the number of consecutive pairs in the data
     x = partial_data.size - 1
 
+    # Create the regression line
     avg_slope = 0
-    intercept = partial_data.mean()
+    intercept = partial_data[0]
 
     for i in range(x):
         avg_slope += partial_data[i + 1] - partial_data[i]
     avg_slope /= x
 
+    # If permitted, graph out the data with the regression line
     if graph:
         partial_data_plot = partial_data.plot(figsize = (8, 5))
         partial_data_plot.plot([intercept + avg_slope * i for i in range(x + 1)])
         partial_data_plot.get_figure().savefig(figname)
 
-    deviation = round(partial_data.std(), 2)
+    # Print out the next predicted value and the deviation (range the actual value could fall on)
+    deviation = None
+    prediction = None
 
-    prediction = round(intercept + avg_slope * (x + 1), 2)
+    ## Volume needs to be a whole number
+    if name == "volume":
+        deviation = round(partial_data.std())
+        prediction = round(intercept + avg_slope * (x + 1))
+    else:
+        deviation = round(partial_data.std(), 2)
+        prediction = round(intercept + avg_slope * (x + 1), 2)
 
     print("(" + sym + ")", name, "next day:", prediction, "+-", deviation)
 
@@ -50,6 +62,6 @@ data = data.astype(float)
 # Reverse all data
 data = data.iloc[::-1]
 
-###### Linear Regression WIP
+###### Regression Techniques WIP
 
 data_linear_regression_graph(symbol, "close", False, "")
